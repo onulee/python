875b8d3f67d62b1c65d3935774c6ecc6f2eb23a7
onulee <onulee@naver.com>
2025-05-02 16:56:41 +0900

    게시글 등록

--- py0502/p0502_02.py
+++ py0502/p0502_02.py
@@ -1,37 +1,47 @@
 from stuConn import *
-
+## 1. db연결
 conn = connections()
 
-## 게시판 글쓰기 1개 저장
+## 2. 게시판 글쓰기 
 title = input("게시글 제목을 입력하세요.>> ")
 content = input("게시글 내용을 입력하세요.>> ")
-# 파일첨부
-bfile = []
-bfile1 = input("첨부할 파일을 입력하세요.( 1.jpg )>> ")
-bfile2 = input("첨부할 파일을 입력하세요.( 2.jpg )>> ")
-bfile3 = input("첨부할 파일을 입력하세요.( 3.jpg )>> ")
+## 3. 파일첨부 등록
+bname = []
+count = 0
+while True:
+    temp = input(f"{count+1}번 첨부할 파일을 입력하세요.( 0.종료 )>> ")
+    if temp=='0': break
+    bname.append(temp)
+    count += 1
+ 
 
+## 4. 시퀀스 번호 생성
 cursor = conn.cursor()
 query = "select board_seq.nextval from dual"
 cursor.execute(query)
 bno = cursor.fetchone()[0] # 9 / #(9,) -> bno[0]
-bfile.append([bno,bfile1])
-bfile.append([bno,bfile2])
-bfile.append([bno,bfile3])
+print("시퀀스번호 생성 : ",bno)
 
-# 1개씩 저장이 아니라 여러개 저장을 하기 위해 리스트형태로 구성
-# [
-#    [9,'1.jpg'],[9,'2.jpg'],[9,'3.jpg']
-# ]
+## 5. 파일 리스트 생성
+#  [9,'1.jpg'],[9,'2.jpg'],[9,'3.jpg']
 
+bfile = []
+for b in bname: bfile.append([bno,b])
+    
+# bfile = [[bno,b] for b in bname] # 리스트내포   
 
-# query = "insert into board values (board_seq.nextval,\
-#         :btitle,:bcontent,'aaa',board_seq.currval,0,0,0,sysdate)"
-# cursor.execute(query,btitle=title,bcontent=content)
-# conn.commit()
+## 6. db에 게시글 저장
+query = "insert into board values (:bno,\
+         :btitle,:bcontent,'aaa',:bno,0,0,0,sysdate)"
+cursor.execute(query,bno=bno,btitle=title,bcontent=content)
+conn.commit()
 
-# 게시글 여러개 저장
-
+## 7. db에 파일첨부 여러개저장
+query = "insert into bfile values (:1,:2)"
+cursor.executemany(query,bfile)
+conn.commit()
 conn.close()
 
+## 8. 프로그램 종료
+print(bfile)
 print("프로그램을 종료합니다.")
