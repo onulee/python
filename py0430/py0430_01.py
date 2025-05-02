12953492788499c5c53904237728b8e8517a671d
onulee <onulee@naver.com>
2025-05-02 18:24:51 +0900

    게시판 글쓰기

--- py0430/py0430_01.py
+++ py0430/py0430_01.py
@@ -10,8 +10,8 @@ cursor = conn.cursor() # sql창 오픈
 # query = "select employee_id,emp_name,salary from employees where salary between 4000 and 6000"
 
 name = input("검색하려는 이름을 입력하세요.>> ")
-name = '%'+name+'%'
-query = "select employee_id,emp_name,salary from employees where emp_name like :name "
+# name = '%'+name+'%'
+query = "select employee_id,emp_name,salary from employees where emp_name like '%||:name||%' "
 cursor.execute(query,name=name) #sql구문 F9실행
 
 rows = cursor.fetchall()  # 데이터를 가져옴.
